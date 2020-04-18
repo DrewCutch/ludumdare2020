66d30f027cf5f898f3c1e43d1d26977f2b36e431
Drew Cutchins <drew.cutch@gmail.com>
2020-04-18 01:27:23 -0400

    Add energy system to actor

--- Keep It Alive/Assets/Scripts/Actor.cs
+++ Keep It Alive/Assets/Scripts/Actor.cs
@@ -2,19 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Damagable))]
 public class Actor : MonoBehaviour
 {
     public Alignment Alignment;
 
+    public int MaxEnergy;
+
+    public int Energy;
+
+    public int EnergyRecovery;
+
+    public Damagable Health { get; private set; }
+
+    private float _recovering;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Energy = 0;
+        Health = gameObject.GetComponent<Damagable>();
+        Health.OnDestroyed += OnDie;
+    }
+
+    private void OnDie()
+    {
+        print("I'm dead!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Energy < MaxEnergy)
+            _recovering += Time.deltaTime * EnergyRecovery;
+
+        if (_recovering > 1)
+        {
+            Energy += (int) _recovering;
+            _recovering -= (int)_recovering;
+        }
+    }
+
+    public bool HasEnergy(int amount) => amount < Energy;
+
+    public bool UseEnergy(int amount)
+    {
+        if (amount > Energy)
+            return false;
+
+        Energy -= amount;
+        return true;
     }
 }
