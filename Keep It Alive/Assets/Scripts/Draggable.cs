43139af5d4cb7df81942e4f1be7d8c8d76ca44c9
Drew Cutchins <drew.cutch@gmail.com>
2020-04-17 23:11:31 -0400

    Create Draggable Component

---
+++ Keep It Alive/Assets/Scripts/Draggable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Draggable : MonoBehaviour
+{
+
+    public Camera MainCamera;
+    public float DragStrength;
+    public float Damping;
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 dist = transform.position - MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        _rb.AddForce(-dist * DragStrength - _rb.velocity * Damping);
+    }
+}
