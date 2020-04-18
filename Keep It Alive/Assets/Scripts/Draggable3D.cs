f42bd0efb3a125eb76d5e12ddbfd25d0155f49b1
Drew Cutchins <drew.cutch@gmail.com>
2020-04-18 14:50:45 -0400

    Create Draggable3D component

---
+++ Keep It Alive/Assets/Scripts/Draggable3D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable3D : MonoBehaviour
+{
+
+    public Camera MainCamera;
+    public float DragStrength;
+    public float Damping;
+
+    public float GroundOffset;
+
+    private Rigidbody _rb;
+    private Plane _worldPlane;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody>();
+        _worldPlane = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        Vector3 target = Vector3.zero;
+        if (_worldPlane.Raycast(ray, out distance))
+        {
+            target = ray.GetPoint(distance) + new Vector3(0, GroundOffset, 0);
+        }
+
+        Vector3 dist = transform.position - target;
+        _rb.AddForce(-dist * DragStrength - _rb.velocity * Damping);
+    }
+}
