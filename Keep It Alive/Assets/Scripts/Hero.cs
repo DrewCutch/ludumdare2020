2eb0d62e0c7f118ddab4725518f8a688f5a432bc
Drew Cutchins <drew.cutch@gmail.com>
2020-04-18 00:08:54 -0400

    Create Initial Hero script

---
+++ Keep It Alive/Assets/Scripts/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ForceMotor))]
+public class Hero : MonoBehaviour
+{
+    public Transform TopOfScreen;
+
+    private ForceMotor _motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _motor = gameObject.GetComponent<ForceMotor>();
+        _motor.SetTarget(TopOfScreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
