5a0fb05c2b3209762d9d298ca862fa465abd61e2
Drew Cutchins <drew.cutch@gmail.com>
2020-04-17 23:59:44 -0400

    Create ForceMotor component

---
+++ Keep It Alive/Assets/Scripts/ForceMotor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ForceMotor : MonoBehaviour
+{
+    private Transform _target;
+    private Vector3 _direction;
+
+    public MotorMode Mode { get; private set; }
+
+    public float Speed;
+    public float MaxSpeed;
+
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Mode == MotorMode.Direction
+            ? _direction
+            : (_target.position - transform.position).normalized;
+        
+        if (_rb.velocity.sqrMagnitude < MaxSpeed * MaxSpeed)
+            _rb.AddForce(direction * Speed);
+    }
+
+    public void SetTarget(Transform transform)
+    {
+        _target = transform;
+        Mode = MotorMode.Target;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        _direction = direction;
+        Mode = MotorMode.Direction;
+    }
+}
