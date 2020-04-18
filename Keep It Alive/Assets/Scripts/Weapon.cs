b2f2b63695259d63a7367022de4cfa99ff84003f
Drew Cutchins <drew.cutch@gmail.com>
2020-04-18 17:17:06 -0400

    Create Weapon Component

---
+++ Keep It Alive/Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public WeaponProfile WeaponType;
+
+    public void Attack(Transform target)
+    {
+        WeaponType.Attack(transform, target);
+    }
+}
