37a1986927225b9c573f19fa406c81d840e37922
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 01:24:45 -0400

    Create JointBreaker component

---
+++ Keep It Alive/Assets/Scripts/JointBreaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBreaker : MonoBehaviour
+{
+    public Joint[] joints;
+
+    public Collider[] colliders;
+
+    public void BreakAll()
+    {
+        foreach (Joint joint in joints)
+        {
+            Destroy(joint);
+        }
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = true;
+        }
+    }
+}
