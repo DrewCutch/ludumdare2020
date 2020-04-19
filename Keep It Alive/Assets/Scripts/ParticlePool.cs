5784de2971b89ee4ee0a766413723581da1c2c57
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 01:24:03 -0400

    Create ParticlePool Component

---
+++ Keep It Alive/Assets/Scripts/ParticlePool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePool : MonoBehaviour
+{
+    public ParticleSystem System;
+
+    void Awake()
+    {
+        System.Play();
+    }
+
+    public void PlayAt(Vector3 position)
+    {
+        print("Play at" + position);
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams {position = position, startSize = 1};
+        System.Emit(emitParams, 1);
+    }
+}
