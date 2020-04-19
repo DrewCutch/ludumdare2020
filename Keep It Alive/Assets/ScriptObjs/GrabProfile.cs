90e19e067e40cb49e797444de56b17103819bd0f
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 15:45:42 -0400

    Fix GrabProfile menu name

--- Keep It Alive/Assets/ScriptObjs/GrabProfile.cs
+++ Keep It Alive/Assets/ScriptObjs/GrabProfile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WeaponProfile", menuName = "ScriptableObjects/WeaponProfile", order = 1)]
+[CreateAssetMenu(fileName = "GrabProfile", menuName = "ScriptableObjects/GrabProfile", order = 1)]
 public class GrabProfile : ScriptableObject
 {
     public float DragStrength;
