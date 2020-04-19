40eb86d827507bb6f43c42dda352bb9d998aa707
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 12:48:49 -0400

    Create Spawner component

---
+++ Keep It Alive/Assets/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Object;
+
+    public Vector3 Velocity;
+
+    public void Spawn()
+    {
+        GameObject created = Instantiate(Object, transform.position, Quaternion.identity);
+        created.GetComponent<Rigidbody>()?.AddForce(Velocity, ForceMode.VelocityChange);
+    }
+}
