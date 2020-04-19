1c2618cd94a40629a394a71925f1093a9148a572
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 17:56:57 -0400

    Create IsoUtils

---
+++ Keep It Alive/Assets/Scripts/IsoUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public static class IsoUtils
+{
+
+    public static Plane plane;
+
+    static IsoUtils()
+    {
+        plane = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    public static Vector3 ScreenToIso(Vector3 mousePosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+        if (plane.Raycast(ray, out var distance))
+            return ray.GetPoint(distance);
+
+        return Vector3.zero;
+    }
+}
