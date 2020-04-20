9aa9214803a4aeba68893da1493b2b05cdfe32a7
Drew Cutchins <drew.cutch@gmail.com>
2020-04-20 00:26:15 -0400

    Create HeroTrigger component

---
+++ Keep It Alive/Assets/Scripts/HeroTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HeroTrigger : MonoBehaviour
+{
+    public GameObject Hero;
+
+    public UnityEvent OnHeroEnter;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject == Hero)
+            OnHeroEnter?.Invoke();
+    }
+}
