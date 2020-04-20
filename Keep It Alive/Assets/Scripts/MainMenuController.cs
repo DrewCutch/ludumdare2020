e51a6157b34bc46303e7a500dce7e9d578fabd8a
Drew Cutchins <drew.cutch@gmail.com>
2020-04-20 00:01:31 -0400

    Update MainMenuController

--- Keep It Alive/Assets/Scripts/MainMenuController.cs
+++ Keep It Alive/Assets/Scripts/MainMenuController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MainScreen;
+
+    public GameObject LevelSelectScreen;
+
+    public GameObject SettingsScreen;
+
+
+    public void NewGame()
     {
-        
+        LevelManager.MainLevelManager.GoToNextLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToMain()
     {
-        
+        MainScreen?.SetActive(true);
+        LevelSelectScreen?.SetActive(false);
+        SettingsScreen?.SetActive(false);
+    }
+
+    public void GoToLevelSelect()
+    {
+        MainScreen?.SetActive(false);
+        LevelSelectScreen?.SetActive(true);
+        SettingsScreen?.SetActive(false);
+    }
+
+    public void GoToSettings()
+    {
+        MainScreen?.SetActive(false);
+        LevelSelectScreen?.SetActive(false);
+        SettingsScreen?.SetActive(true);
     }
 }
