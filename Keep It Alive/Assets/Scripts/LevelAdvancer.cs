943515e7d24d9c8d72e542c7e731ab0b320c6645
Drew Cutchins <drew.cutch@gmail.com>
2020-04-20 00:25:51 -0400

    Create LevelAdvancer Component

---
+++ Keep It Alive/Assets/Scripts/LevelAdvancer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LevelAdvancer : MonoBehaviour
+{
+    public void AdvanceLevel()
+    {
+        LevelManager.MainLevelManager.GoToNextLevel();
+    }
+}
