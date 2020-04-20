09596d22e62f8cb85efc13c2f15492de4e6c5a63
Drew Cutchins <drew.cutch@gmail.com>
2020-04-20 10:50:13 -0400

    Create HealthItem script

---
+++ Keep It Alive/Assets/Scripts/HealthItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthItem : MonoBehaviour
+{
+    public int HealAmount;
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.GetComponent<Damagable>()?.Heal(HealAmount);
+    }
+}
