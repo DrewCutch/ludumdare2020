0cfa681a9aa48acabb1ac8ef286c1bb0e251b3b6
Drew Cutchins <drew.cutch@gmail.com>
2020-04-19 20:49:38 -0400

    Create KillZone component

---
+++ Keep It Alive/Assets/Scripts/KillZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Damagable>() is Damagable damagable)
+            damagable.Damage(int.MaxValue);
+    }
+}
