aa1c5f9a9f5ca9addb15aca616ddc5fcff4cd3cf
Drew Cutchins <drew.cutch@gmail.com>
2020-04-20 00:26:57 -0400

    Update LevelManager

--- Keep It Alive/Assets/Scripts/LevelManager.cs
+++ Keep It Alive/Assets/Scripts/LevelManager.cs
@@ -1,24 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public String[] Levels;
+
+    public int CurrentLevelI { get; private set; }
+
+    public static LevelManager MainLevelManager;
+
+    public void Awake()
+    {
+        if (MainLevelManager != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        GameObject.DontDestroyOnLoad(gameObject);
+
+        MainLevelManager = this;
+        CurrentLevelI = Levels.ToList().IndexOf(SceneManager.GetActiveScene().name);
+    }
 
     public void RestartLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        SceneManager.LoadScene(Levels[CurrentLevelI]);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToNextLevel()
     {
-        
+        CurrentLevelI = (CurrentLevelI + 1) % Levels.Length;
+        RestartLevel();
+    }
+
+    public void GoToMainMenu()
+    {
+        CurrentLevelI = 0;
+        RestartLevel();
     }
 }
